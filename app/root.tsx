e2cd3ed12261047f65f5903e57c03feaf7e340ae
XinyuFeng <xinyufeng16@hotmail.com>
2024-07-14 15:36:22 -0700

    code

---
+++ app/root.tsx
@@ -0,0 +1,45 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  LiveReload,
+} from "@remix-run/react";
+import "./tailwind.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalStylesUrl from "./styles/global.css";
+import { LinksFunction } from "@remix-run/react/dist/routeModules";
+import JokesIndexRoute from "./routes/jokes._index";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+  {
+    rel: "stylesheet",
+    href: globalMediumStylesUrl,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStylesUrl,
+    media: "screen and (min-width: 1024px)",
+  },
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Remix: so great, it&apos;s funny!</title>
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
